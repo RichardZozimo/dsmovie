c11300a165aaa5f06f90bb690762ef3395f1b2e6
Richard Zozimo <richardzozimo@gmail.com>
2022-03-10 07:37:27 -0300

    Movie card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,32 @@
-﻿import Pagination from "components/Pagination";
+﻿import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
 
 export default function Listing() {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
